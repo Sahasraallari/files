345ba482448d99dd33ab860f189930c77ce2e0ef
Sahasraallari <mohan.velagala@gmail.com>
2025-04-04 17:41:16 +0530

    games

---
+++ game.py
@@ -0,0 +1,28 @@
+import random
+def computer():
+    return random. choice(['rock','paper,''scissors'])
+def get_winner(player,computer):
+    if player == computer:
+        return "it's a tie"
+    elif(player=='rock' and computer == 'sicssors')or\
+     (player =='sicssors' and computer == 'paper')or\
+     (player == 'paper' and computer == 'rock'):
+        return "You win"
+    else:
+        return "Computer wins"
+def main():
+    while True:
+        Player_choice=input("Enter rock,paper or scissors(or 'quit'to exit):").lower()
+        if Player_choice == 'quit':
+            print("Thanks for playing")
+            break
+        if Player_choice not in[ 'rock','paper','scissors']:
+            print(" Invald choice,try again.")
+            continue
+        computer_choice = computer()
+        print(f"Computer choose:{computer_choice}")
+        print(get_winner(Player_choice, computer_choice))
+if __name__ == "__main__":
+     main()
+
+
